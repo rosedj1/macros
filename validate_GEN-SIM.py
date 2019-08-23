c15b504e7f73d8194c396e19357435e86e6fd45b
Jake <rosedj1@gmail.com>
2019-08-23 12:13:25 -0400

    Removing README and updating validate_GEN-SIM.py

--- validate_GEN-SIM.py
+++ validate_GEN-SIM.py
@@ -1,21 +1,48 @@
+#!/bin/bash
+#####################################################################################################
+## PURPOSE: 
+## SYNTAX:  
+## NOTES:   
+## AUTHOR:  
+## DATE:    
+## UPDATED: 
+#####################################################################################################
+
+#____________________________________________________________________________________________________
+# User Parameters
 import ROOT
+import sys
 from DataFormats.FWLite import Events, Handle
 import numpy as np
 #import matplotlib.pyplot as plt
+#___________________________________________________________________________
+# User Parameters
+onTier2 = True
 
-events = Events('zd0j_MZd30_eps1e-3_LHE-GEN-SIM_57.root')
-handle = Handle('std::vector<reco::GenParticle>')
+#___________________________________________________________________________
+# Automatic Stuff
+t2_prefix = "root://cmsio5.rc.ufl.edu/"
+
+if onTier2:
+    inputfile = str(t2_prefix+sys.argv[1])
+else:
+    inputfile = str(sys.argv[1])
+openfile = TFile.Open(inputfile,"READ")
+
+events = Events(openfile)
+## Handle(Type). Example Handles:
+#handle = Handle('std::vector<reco::GenParticle>')
+handle = Handle('vector<reco::GenParticle>')
 label = ("genParticles","","SIM") 
-numProcess = 0
 
-### Each item in this for loop is an entire gg-->H-->ZZd-->4lep process ###
+# Each item in this for loop is an entire gg-->H-->ZZd-->4lep process ###
+numProcess = 0
 for count,e in enumerate(events, 1):
-# Only look at first "count" events
-    if count > 100: continue
+    if count > 100: break
     e.getByLabel(label,handle)
     genParticles = handle.product()
     numProcess+=1
-### Each item in this for loop is a single particle in the process above ###
+# Each item in this for loop is a single particle in the process above ###
     for p in genParticles:
         if not p.isHardProcess(): continue
         #print p.pdgId(),p.pt(),p.eta()
